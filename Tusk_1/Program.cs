748d29b2103365edaae8878789d5e6efa0f990d3
Skadi <fant1maz@yandex.ru>
2022-07-14 21:05:35 +0300

    Homework

---
+++ Tusk_1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа
+*/
+Console.WriteLine("Введите трехзначное число");
+while (true)
+{
+    int number = int.Parse(Console.ReadLine());
+    if (number > 99 && number < 1000)
+    {
+        int result = (number % 100) / 10;
+        Console.WriteLine($"Вторым числом в {number} является число {result}");
+        break;
+    }
+    Console.WriteLine("Вы ввели не корректное число. Введите трехзначное число");
+}
+
---
+++ Tusk_2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+*/
+Console.WriteLine("Введите число");
+
+string number = Console.ReadLine();
+
+char[] numberArray = number.ToCharArray();
+
+if (numberArray.Length >= 3){
+    Console.WriteLine($"Третим числом в {number} является число {numberArray[2]}");
+} else {
+    Console.WriteLine("Третьей цифры нет");
+}
